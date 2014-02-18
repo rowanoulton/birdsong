19a3d14ca170c19c5df5a93993726fadc1b35909
Rowan Oulton <rowan.oulton@gmail.com>
2014-02-18 22:35:38 +1300

    Added models

---
+++ models/bird.js
@@ -0,0 +1,63 @@
+/**
+ * Dependencies
+ */
+var XenoCanto = require('xeno-canto'),
+    _         = require('underscore');
+
+/**
+ * Constructor
+ */
+
+/*
+ * @class Bird
+ * @constructor
+ * @param {Object} args Search parameters for XenoCanto API. See https://github.com/tripitakit/xeno-canto
+ */
+function Bird (args) {
+    this.recordings = [];
+    this.api        = new XenoCanto();
+    this.args       = args;
+};
+
+/**
+ * Methods
+ */
+
+/*
+ * Return a property of the search arguments
+ *
+ * @method get
+ * @param  {String} property argument key
+ * @return {Mixed}
+ */
+Bird.prototype.get = function (property) {
+    return this.args[property];
+};
+
+/*
+ * Load recordings from the XenoCanto API and store them
+ *
+ * @param {Function} callback
+ */
+Bird.prototype.load = function (callback) {
+    this.api.search(this.args, function (data) {
+        this.recordings = data.entity.recordings;
+        console.log(this.recordings.length + ' recordings loaded for ' + this.get('name'));
+        callback();
+    }.bind(this));
+};
+
+/*
+ * Return a random(!) entry from stored list of recordings
+ *
+ * @method getRandomRecording
+ * @return {Object} recording
+ */
+Bird.prototype.getRandomRecording = function () {
+    return _.sample(this.recordings);
+};
+
+/**
+ * Export
+ */
+module.exports = Bird;
---
+++ models/flock.js
@@ -0,0 +1,71 @@
+/**
+ * Dependencies
+ */
+var _ = require('underscore');
+
+/**
+ * Constructor
+ */
+
+/*
+ * @class Flock
+ * @constructor
+ * @param {Array} [birds] Optional array of birds to begin with
+ */
+function Flock (birds) {
+    this.birds = [];
+
+    if (_.isArray(birds)) {
+        this.add(birds);
+    }
+};
+
+/**
+ * Methods
+ */
+
+/*
+ * Get a random(!) bird from the flocks collection
+ *
+ * @method getRandom
+ * @return {Bird}
+ */
+Flock.prototype.getRandom = function () {
+    return _.sample(this.birds);
+};
+
+/*
+ * Add one of many birds to the collection
+ *
+ * @method add
+ * @param {Array|Bird} data
+ */
+Flock.prototype.add = function (data) {
+    if (_.isArray(data)) {
+        _.each(data, function (bird) {
+            this._add(bird);
+        }.bind(this));
+    } else if (_.isObject(data)) {
+        this._add(data);
+    }
+};
+
+/*
+ * Adds a single instance of a Bird model to internal collection
+ *
+ * @private
+ * @method _add
+ * @param {Bird} bird
+ */
+Flock.prototype._add = function (bird) {
+    this.birds.push(bird);
+
+    // @todo: Trigger "bird added" custom event, or implement extension of a
+    // backbone collection if it would suite
+    console.log('Bird added: ' + bird.get('name'));
+};
+
+/**
+ * Export
+ */
+module.exports = Flock;
